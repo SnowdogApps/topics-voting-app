dea0ac2f7c70d30ddf7d507f40b1a9a50470838b
Zuza <zuzanna.szymanda@snow.dog>
2020-07-23 12:15:25 +0200

    lang switcher update

--- src/components/LangSwitcher.vue
+++ src/components/LangSwitcher.vue
@@ -1,83 +1,50 @@
 <template>
-  <div class="lang-switcher">
-    <div class="lang-switcher__select select">
-      <select
-        class="select__field"
-        v-model="$i18n.locale"
-        @change="saveLang"
-      >
-        <option
-          v-for="(lang, i) in langs"
-          :key="`Lang${i}`"
-          :value="lang"
-        >{{ lang }}</option>
-      </select>
-    </div>
+  <div class="dropdown">
+    <button
+      :class="['dropdown__button', { 'dropdown__button--active': show }]"
+      type="button"
+      @click="toggle"
+    >
+      {{ $i18n.locale.toUpperCase() }}
+    </button>
+
+    <transition name="fade">
+      <div v-show="show" class="dropdown__content">
+        <div
+          v-for="lang in langs"
+          :key="lang"
+          class="dropdown__link"
+          @click="saveLang(lang)"
+        >
+          {{ lang }}
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return { langs: ['en', 'pl'] }
+    return {
+      langs: ['en', 'pl'],
+      show: false
+    }
   },
   methods: {
-    saveLang () {
+    saveLang (lang) {
+      this.$i18n.locale = lang
       if (typeof localStorage === 'undefined') {
         return
       }
-      const currentLang = this.$i18n.locale
-      localStorage.setItem('lang', currentLang)
+      localStorage.setItem('lang', lang)
+    },
+    toggle () {
+      this.show = !this.show
+    },
+    hide () {
+      this.show = false
     }
   }
 }
 </script>
-<style lang="scss" scoped>
-.lang-switcher {
-  display: flex;
-  align-items: center;
-  margin-right: 16px;
-
-  &__select {
-    margin: 0 0 0 auto;
-  }
-}
-.select {
-  position: relative;
-  width: 72px;
-
-  &:after {
-    content: '';
-    top: 45%;
-    right: 10px;
-    position: absolute;
-    display: block;
-    width: 0;
-    height: 0;
-    border-left: 6px solid transparent;
-    border-right: 6px solid transparent;
-    border-top: 6px solid $gray-light;
-  }
-  &__field {
-    height: 48px;
-    min-width: 72px;
-    padding: $spacer--xs;
-    border: none;
-    border-radius: 0;
-    appearance: none;
-    background: $white;
-    text-align: center;
-    font-family: $font-family-base;
-    line-height: $line-height;
-    font-size: $font-size-small;
-    transition: $transition-base;
-    text-transform: uppercase;
-    cursor: pointer;
-
-    &:disabled {
-      background-color: $gray-lighter;
-      cursor: not-allowed;
-    }
-  }
-}
-</style>
